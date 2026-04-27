679dba8993175831e89734a70e147037b07648c1
renshei <100230983@student.burnley.ac.uk>
2026-04-27 10:26:56 +0100

    package and vercel yayayayayaya

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,4 +1,6 @@
-export default async function handler(req: Request, res: Response) {
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
     res.setHeader('Access-Control-Allow-Origin', "*"); // where you can access source from
     res.setHeader('Access-Control-Allow-Methods', "GET, OPTIONS"); // allowed methods
     res.setHeader('Access-Control-Allow-Headers', "Content-Type"); //type of headers allowed
@@ -9,7 +11,7 @@ export default async function handler(req: Request, res: Response) {
 
     console.log(req)
     console.log(res)
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/plaintext');
     res.write('<button>hi guys</button>'); // creates it on page
     return res.end(); // ends the response
 }
