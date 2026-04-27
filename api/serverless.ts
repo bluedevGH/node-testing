ed14d61f02876dc2f4ef3fcb81f381c90fe10e92
renshei <100230983@student.burnley.ac.uk>
2026-04-27 10:22:32 +0100

    heeheeheehaw

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,19 +1,15 @@
-export async function GET(req: Request, res: Response) {
+export default async function handler(req: Request, res: Response) {
     res.setHeader('Access-Control-Allow-Origin', "*"); // where you can access source from
     res.setHeader('Access-Control-Allow-Methods', "GET, OPTIONS"); // allowed methods
     res.setHeader('Access-Control-Allow-Headers', "Content-Type"); //type of headers allowed
+
+    if (req.method === "OPTIONS") {
+        return res.statusCode(200);
+    }
 
     console.log(req)
     console.log(res)
     res.setHeader('Content-Type', 'text/html');
     res.write('<button>hi guys</button>'); // creates it on page
     return res.end(); // ends the response
-}
-
-export async function OPTIONS(req: Request, res: Response) {
-    res.setHeader('Access-Control-Allow-Origin', "*"); // where you can access source from
-    res.setHeader('Access-Control-Allow-Methods', "GET, OPTIONS"); // allowed methods
-    res.setHeader('Access-Control-Allow-Headers', "Content-Type"); //type of headers allowed
-
-    return res.statusCode(200);
 }
