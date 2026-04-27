4cf42196581c4e50c15ad287de29d0bc801bc3e5
renshei <100230983@student.burnley.ac.uk>
2026-04-27 10:08:17 +0100

    asyncccccccccccc

--- api/serverless.js
+++ api/serverless.js
@@ -1,4 +1,4 @@
-export function GET(req, res) {
+export async function GET(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.write('<button>hi guys</button>'); // creates it on page
     res.end(); // ends the response
